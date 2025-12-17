0ff1aba563a58b19b0d6b98e82fc3da8c69a210d
jun_sajima <jun_sajima@users.noreply.github.com>
2025-12-17 01:46:55 +0000

    Azureで動かす前にコミット

--- src/work_1.py
+++ src/work_1.py
@@ -3,5 +3,89 @@ import marimo
 __generated_with = "0.18.4"
 app = marimo.App()
 
+
+@app.cell
+def _():
+    from typing import Annotated, TypedDict
+    import operator
+    from langgraph.graph import StateGraph, END
+    import functools
+    return END, StateGraph, functools
+
+
+app._unparsable_cell(
+    r"""
+    class PiggyBankState(TypedDict):
+        total: Annotated[int, o       perator.add]
+        count: Annotated[int, operator.add]
+        last_deposit: int
+    """,
+    name="_"
+)
+
+
+@app.cell
+def _(PiggyBankState):
+    def deposit(state: PiggyBankState) -> dict:
+        amount = int(input("Enter the amount to deposit: "))
+        return {"total": amount, "count": 1, "last_deposit": amount}
+    return (deposit,)
+
+
+@app.cell
+def _(PiggyBankState):
+    def finalize(state: PiggyBankState) -> dict:
+        print(f"{state['count']}回の貯金で目標金額に到達しました。")
+        print(f"{state['total']}円貯まっています。")
+        print(f"最後の入金額は{state['last_deposit']}円でした。")
+        return {"total": 0}
+    return (finalize,)
+
+
+@app.cell
+def _(PiggyBankState):
+    def check_goal(state: PiggyBankState, goal: int) -> dict:
+        if state["total"] >= goal:
+            return "full"
+        else:
+            return "continue"
+
+    return (check_goal,)
+
+
+@app.cell
+def _(
+    END,
+    PiggyBankState,
+    StateGraph,
+    check_goal,
+    deposit,
+    finalize,
+    functools,
+):
+    def piggy_bank(goal: int):
+        workflow = StateGraph(PiggyBankState)
+        workflow.add_node("Deposit", deposit)
+        workflow.add_node("Full", finalize)
+        workflow.add_conditional_edges(
+            "Deposit",
+            functools.partial(check_goal, goal=goal),
+            {"continue": "Deposit", "full": "Full"},
+        )
+        workflow.add_edge("Full", END)
+        workflow.set_entry_point("Deposit")
+        graph = workflow.compile()
+        final_state = graph.invoke({"total": 0, "count": 0, "last_deposit": 0})
+        print(final_state)
+    return (piggy_bank,)
+
+
+@app.cell
+def _(piggy_bank):
+    if __name__ == "__main__":
+        piggy_bank(1000)
+    return
+
+
 if __name__ == "__main__":
     app.run()
---
+++ src/work_2.py
@@ -0,0 +1,127 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import operator
+    from typing import TypedDict, Annotated, Sequence
+    from langchain_core.messages import BaseMessage, ToolMessage, HumanMessage
+    from langchain_openai import ChatOpenAI
+    from langgraph.graph import END, StateGraph
+    from langchain_core.tools import tool
+    import subprocess
+    return (
+        Annotated,
+        BaseMessage,
+        ChatOpenAI,
+        END,
+        HumanMessage,
+        Sequence,
+        StateGraph,
+        ToolMessage,
+        TypedDict,
+        operator,
+        subprocess,
+        tool,
+    )
+
+
+@app.cell
+def _(subprocess, tool):
+    @tool
+    def exec_command(shell_command: str) -> str:
+        """シェルコマンドを実行します。
+        shell_command: Linuxシェルコマンド
+        """
+        result = subprocess.run(shell_command, shell=True, capture_output=True)
+        return result.stdout.decode("utf-8") + result.stderr.decode("utf-8")
+    return (exec_command,)
+
+
+@app.cell
+def _(Annotated, BaseMessage, Sequence, TypedDict, operator):
+    class AgentState(TypedDict):
+        messages: Annotated[Sequence[BaseMessage], operator.add]
+    return (AgentState,)
+
+
+@app.cell
+def _(ChatOpenAI, exec_command):
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    llm_with_tool = llm.bind_tools([exec_command])
+    return (llm_with_tool,)
+
+
+@app.cell
+def _(AgentState, llm_with_tool):
+    def agent_node(state: AgentState):
+        messages = state["messages"]
+        response = llm_with_tool.invoke(messages)
+        return {"messages": [response]}
+    return (agent_node,)
+
+
+@app.cell
+def _(AgentState, ToolMessage, exec_command):
+    def tool_node(state: AgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        messages = []
+        for call in last_message.tool_calls:
+            if call["name"] == "exec_command":
+                value = exec_command.invoke(call["args"])
+                tool_message = ToolMessage(
+                    content=value,
+                    name=call["name"],
+                    tool_call_id=call["id"],
+                )
+                messages.append(tool_message)
+        return {"messages": messages}
+
+    return (tool_node,)
+
+
+@app.cell
+def _(AgentState):
+    def should_continue(state: AgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        if last_message.tool_calls:
+            return "tool"
+        else:
+            return "end"
+    return (should_continue,)
+
+
+@app.cell
+def _(AgentState, END, StateGraph, agent_node, should_continue, tool_node):
+    workflow = StateGraph(AgentState)
+    workflow.add_node("Agent", agent_node)
+    workflow.add_node("Tool", tool_node)
+    workflow.add_conditional_edges(
+        "Agent",
+        should_continue,
+        {
+            "tool": "Tool",
+            "end": END,
+        },
+    )
+    workflow.add_edge("Tool", "Agent")
+    workflow.set_entry_point("Agent")
+    graph = workflow.compile()
+    return (graph,)
+
+
+@app.cell
+def _(HumanMessage, graph):
+    query = input("query: ")
+    state = graph.invoke({"messages": [HumanMessage(content=query)]})
+    print(state["messages"][-1].content)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
